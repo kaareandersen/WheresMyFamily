19f63cfe91b96973099011d0aa3b528b579b064d
TommyIsGod <tommyvhansens@gmail.com>
2014-11-10 11:21:04 +0100

    Merge branch 'master' of https://github.com/kaareandersen/WheresMyFamily
    Conflicts:
    	app/src/main/java/dk/projekt/bachelor/wheresmyfamily/activities/LoggedInParent.java
    	app/src/main/res/layout/activity_logged_in_parent.xml

---
+++ app/src/main/java/dk/projekt/bachelor/wheresmyfamily/LocationService.java
@@ -0,0 +1,83 @@
+package dk.projekt.bachelor.wheresmyfamily;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
+public class LocationService extends Service implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static final String TAG = "LocationService";
+    private boolean currentlyProcessingLocation = false;
+    private LocationRequest locationRequest;
+    private LocationClient locationClient;
+
+    public LocationService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (!currentlyProcessingLocation) {
+            currentlyProcessingLocation = true;
+            startTracking();
+        }
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        if(location != null)
+            System.out.println("position: " + location.getLatitude() + ", " + location.getLongitude() + " accuracy: " + location.getAccuracy());
+            //Log.e(TAG, "position: " + location.getLatitude() + ", " + location.getLongitude() + " accuracy: " + location.getAccuracy());
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult)
+    {
+        Log.e(TAG, "onConnectionFailed");
+    }
+
+    private void startTracking() {
+        Log.d(TAG, "startTracking");
+
+        if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS) {
+            locationClient = new LocationClient(this,this,this);
+
+            if (!locationClient.isConnected() || !locationClient.isConnecting()) {
+                locationClient.connect();
+            }
+        } else {
+            Log.e(TAG, "unable to connect to google play services.");
+        }
+    }
+}
