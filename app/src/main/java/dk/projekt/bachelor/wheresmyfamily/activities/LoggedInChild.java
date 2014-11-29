0981bd10103f4764f8f2d0dbbb7fda1db2955a6a
TommyIsGod <tommyvhansens@gmail.com>
2014-11-29 16:47:44 +0100

    Add new place design

--- app/src/main/java/dk/projekt/bachelor/wheresmyfamily/activities/LoggedInChild.java
+++ app/src/main/java/dk/projekt/bachelor/wheresmyfamily/activities/LoggedInChild.java
@@ -1,14 +1,13 @@
 package dk.projekt.bachelor.wheresmyfamily.activities;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Criteria;
+import android.content.IntentSender;
 import android.location.Location;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,9 +16,13 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.location.LocationRequest;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.microsoft.windowsazure.messaging.NotificationHub;
@@ -30,18 +33,18 @@ import org.json.JSONArray;
 
 import java.util.ArrayList;
 
-import dk.projekt.bachelor.wheresmyfamily.DataModel.Parent;
+import dk.projekt.bachelor.wheresmyfamily.Controller.MobileServicesClient;
 import dk.projekt.bachelor.wheresmyfamily.Controller.NotificationHubController;
 import dk.projekt.bachelor.wheresmyfamily.Controller.PushNotificationController;
+import dk.projekt.bachelor.wheresmyfamily.DataModel.Parent;
 import dk.projekt.bachelor.wheresmyfamily.R;
-import dk.projekt.bachelor.wheresmyfamily.Services.LocationService;
 import dk.projekt.bachelor.wheresmyfamily.UserInfoStorage;
-import dk.projekt.bachelor.wheresmyfamily.Controller.MobileServicesClient;
 import dk.projekt.bachelor.wheresmyfamily.authenticator.AuthenticationApplication;
 import dk.projekt.bachelor.wheresmyfamily.helper.BaseActivity;
 
-
-public class LoggedInChild extends BaseActivity implements LocationListener {
+public class LoggedInChild extends BaseActivity implements GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener, LocationClient.OnAddGeofencesResultListener,
+        LocationListener {
 
     //region Fields
     private final String TAG = "LoggedInChild";
@@ -49,9 +52,34 @@ public class LoggedInChild extends BaseActivity implements LocationListener {
     protected PushNotificationController pushNotificationController;
     private TextView mLblUsernameValue,parentInfoName, parentInfoPhone;
     Parent parent = new Parent();
-    private String provider, cEmail;
-    LocationManager locationManager;
+    private String cEmail;
     Location currentLocation;
+    LocationClient locationClient;
+    // Define an object that holds accuracy and frequency parameters
+    private LocationRequest mLocationRequest;
+
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    // Setup Location update interval
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    private static final int UPDATE_INTERVAL_IN_SECONDS = 10;
+    private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 10;
+    private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+
+    /*
+     * Use to set an expiration time for a geofence. After this amount
+     * of time Location Services will stop tracking the geofence.
+     */
+    private static final long SECONDS_PER_HOUR = 60;
+    private static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+    private static final long GEOFENCE_EXPIRATION_TIME =
+            GEOFENCE_EXPIRATION_IN_HOURS * SECONDS_PER_HOUR * MILLISECONDS_PER_SECOND;
+
+    // Constants that define the activity detection interval
+    public static final int DETECTION_INTERVAL_SECONDS = 20;
+    public static final int DETECTION_INTERVAL_MILLISECONDS =
+            MILLISECONDS_PER_SECOND * DETECTION_INTERVAL_SECONDS;
 
     private String SENDER_ID = "911215571794";
     private GoogleCloudMessaging mGcm;
@@ -66,6 +94,7 @@ public class LoggedInChild extends BaseActivity implements LocationListener {
     String childrenKey = "childrenInfo";
     String parentsKey = "parentsInfo";
     UserInfoStorage storage = new UserInfoStorage();
+    LocationActivity locationActivity;
     //endregion
 
     @Override
@@ -83,11 +112,6 @@ public class LoggedInChild extends BaseActivity implements LocationListener {
         mLblUsernameValue = (TextView) findViewById(R.id.lblUsernameValue);
         parentInfoName = (TextView) findViewById(R.id.parentinput);
         parentInfoPhone = (TextView) findViewById(R.id.phoneinput);
-
-        // Toast.makeText(this, "LoggedInChild OnCreate", Toast.LENGTH_SHORT).show();
-
-        MapFragment mapFragment = MapFragment.newInstance();
-        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
 
         // Get parent info
         mParents = storage.loadParents(this);
@@ -119,17 +143,21 @@ public class LoggedInChild extends BaseActivity implements LocationListener {
                }
            });
 
-        // Setup location using  Google's location manager
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        // Initialize the location
+        if (locationClient == null)
+            locationClient = new LocationClient(this, this, this);
 
-        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
-        {
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(intent);
-        }
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 10 seconds
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        // Set the fastest update interval to 10 seconds
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
-        Intent intent = new Intent(this, LocationService.class);
-        startService(intent);
+        locationClient.requestLocationUpdates(mLocationRequest, this);
 
         ImageButton button = (ImageButton) findViewById(R.id.callchild);
         button.setOnClickListener(new View.OnClickListener() {
@@ -160,19 +188,7 @@ public class LoggedInChild extends BaseActivity implements LocationListener {
             parentInfoPhone.setText(mParents.get(0).getPhone());
         }
 
-        // Define the criteria on how to select the location provider
-        Criteria criteria = new Criteria();
-        provider =  locationManager.getBestProvider(criteria, false);
-
-        currentLocation = locationManager.getLastKnownLocation(provider);
-
-        if(currentLocation != null)
-        {
-            System.out.println("Udbyder " + provider + " er valgt");
-            onLocationChanged(currentLocation);
-        }
-        else
-            Toast.makeText(this, "Position utilgængelig", Toast.LENGTH_SHORT).show();
+        locationClient.connect();
     }
 
     @Override
@@ -207,11 +223,7 @@ public class LoggedInChild extends BaseActivity implements LocationListener {
         }
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
 
-        currentLocation = location;
-    }
 
     public void deleteDialogBox(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -249,6 +261,70 @@ public class LoggedInChild extends BaseActivity implements LocationListener {
         String location = currentLocation.toString();
 
         pushNotificationController.sendLocationFromChild(parentEmail, location);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        currentLocation = location;
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        Toast.makeText(this, "LocationActivity connected", Toast.LENGTH_SHORT).show();
+
+        locationClient.requestLocationUpdates(mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onDisconnected() {
+
+        locationClient = null;
+
+    }
+
+    @Override
+    public void onAddGeofencesResult(int i, String[] strings) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        if (connectionResult.hasResolution())
+        {
+            try
+            {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                * Thrown if Google Play services canceled the original
+                * PendingIntent
+                */
+            }
+            catch (IntentSender.SendIntentException e)
+            {
+                // Log the error
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            /*
+            * If no resolution is available, display a dialog to the
+            * user with the error.
+            */
+            // Get the error code
+            int errorCode = connectionResult.getErrorCode();
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(errorCode, this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+
+        }
+
     }
 
     /*public void saveParent(Parent parent)
--- app/src/main/java/dk/projekt/bachelor/wheresmyfamily/activities/LoggedInParent.java
+++ app/src/main/java/dk/projekt/bachelor/wheresmyfamily/activities/LoggedInParent.java
@@ -2,10 +2,13 @@ package dk.projekt.bachelor.wheresmyfamily.activities;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,8 +21,15 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.location.ActivityRecognitionClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.microsoft.windowsazure.messaging.NotificationHub;
@@ -32,18 +42,21 @@ import org.json.JSONArray;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import dk.projekt.bachelor.wheresmyfamily.BroadCastReceiver.MyHandler;
 import dk.projekt.bachelor.wheresmyfamily.Controller.MobileServicesClient;
 import dk.projekt.bachelor.wheresmyfamily.Controller.NotificationHubController;
 import dk.projekt.bachelor.wheresmyfamily.DataModel.Child;
 import dk.projekt.bachelor.wheresmyfamily.R;
-import dk.projekt.bachelor.wheresmyfamily.Services.LocationService;
+import dk.projekt.bachelor.wheresmyfamily.Services.ReceiveTransitionsIntentService;
 import dk.projekt.bachelor.wheresmyfamily.UserInfoStorage;
 import dk.projekt.bachelor.wheresmyfamily.authenticator.AuthenticationApplication;
 
 
-public class LoggedInParent extends ListActivity {
+public class LoggedInParent extends ListActivity implements GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener, LocationClient.OnAddGeofencesResultListener,
+        LocationListener {
 
     //region Fields
     private final String TAG = "LoggedInParent";
@@ -66,6 +79,7 @@ public class LoggedInParent extends ListActivity {
     private GoogleCloudMessaging mGcm;
     private NotificationHub mHub;
     private String mRegistrationId;
+    LocationClient locationClient;
 
     ArrayList<Child> mChildren = new ArrayList<Child>();
     JSONArray mParents = new JSONArray();
@@ -74,6 +88,46 @@ public class LoggedInParent extends ListActivity {
     String childrenKey = "childrenInfo";
     String parentsKey = "parentsInfo";
     UserInfoStorage storage = new UserInfoStorage();
+    // Define an object that holds accuracy and frequency parameters
+    private LocationRequest mLocationRequest;
+
+    // Setup Location update interval
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    private static final int UPDATE_INTERVAL_IN_SECONDS = 10;
+    private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 10;
+    private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+
+    /*
+     * Use to set an expiration time for a geofence. After this amount
+     * of time Location Services will stop tracking the geofence.
+     */
+    private static final long SECONDS_PER_HOUR = 60;
+    private static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+    private static final long GEOFENCE_EXPIRATION_TIME =
+            GEOFENCE_EXPIRATION_IN_HOURS * SECONDS_PER_HOUR * MILLISECONDS_PER_SECOND;
+
+    // Constants that define the activity detection interval
+    public static final int DETECTION_INTERVAL_SECONDS = 20;
+    public static final int DETECTION_INTERVAL_MILLISECONDS =
+            MILLISECONDS_PER_SECOND * DETECTION_INTERVAL_SECONDS;
+
+    // Stores the PendingIntent used to request geofence monitoring
+    private PendingIntent mGeofenceRequestIntent;
+    // Defines the allowable request types.
+    public enum REQUEST_TYPE {ADD, REMOVE_INTENT, REMOVE_LIST, START, STOP }
+    private REQUEST_TYPE mRequestType;
+    // Flag that indicates if a request is underway.
+    private boolean mInProgress;
+    // Store the list of geofence Ids to remove
+    List<String> mGeofencesToRemove;
+
+    // Store the PendingIntent used to send activity recognition events back to the app
+    private PendingIntent mActivityRecognitionPendingIntent;
+    // Store the current activity recognition client
+    private ActivityRecognitionClient mActivityRecognitionClient;
+
     //endregion
 
     @Override
@@ -139,8 +193,22 @@ public class LoggedInParent extends ListActivity {
             }
         });
 
-        Intent intent = new Intent(this, LocationService.class);
-        startService(intent);
+        locationClient = new LocationClient(this, this, this);
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 10 seconds
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        // Set the fastest update interval to 10 seconds
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+
+
+        /*Intent intent = new Intent(this, LocationService.class);
+        startService(intent);*/
     }
 
     @Override
@@ -152,13 +220,14 @@ public class LoggedInParent extends ListActivity {
         // If any children are registered
         if(m_My_children.size() > 0)
         {
-            // Since we are on the home page, set current user to none
+            // Since we are on the home page, set current child to none
             for(int i = 0; i < m_My_children.size(); i++)
             {
                 m_My_children.get(i).setIsCurrent(false);
             }
         }
 
+        locationClient.connect();
 
 
     }
@@ -189,15 +258,157 @@ public class LoggedInParent extends ListActivity {
         runOnUiThread(returnRes);
     }
 
-    private class ChildAdapter extends ArrayAdapter<Child>{
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "LocationActivity connected", Toast.LENGTH_SHORT).show();
 
-        public ChildAdapter(Context context, int textViewResourceId, ArrayList<Child> items){
+        locationClient.requestLocationUpdates(mLocationRequest, this);
+
+        /*if (mRequestType != null)
+        {
+            switch (mRequestType)
+            {
+                case ADD:
+                    mGeofenceRequestIntent = getTransitionPendingIntent();
+                    // Send a request to add the current geofences
+                    locationClient.addGeofences(mCurrentGeofences, mGeofenceRequestIntent, this);
+                case REMOVE_INTENT:
+                    mGeofenceRequestIntent = getTransitionPendingIntent();
+                    locationClient.removeGeofences(mGeofenceRequestIntent,
+                            (LocationClient.OnRemoveGeofencesResultListener) this);
+                    break;
+                case REMOVE_LIST:
+                    locationClient.removeGeofences(mGeofencesToRemove,
+                            (LocationClient.OnRemoveGeofencesResultListener) this);
+                    break;
+                case START :
+                    *//*
+                     * Request activity recognition updates using the
+                     * preset detection interval and PendingIntent.
+                     * This call is synchronous.
+                     *//*
+                    mActivityRecognitionClient.requestActivityUpdates(DETECTION_INTERVAL_MILLISECONDS,
+                            mActivityRecognitionPendingIntent);
+                    break;
+                case STOP :
+                    mActivityRecognitionClient.removeActivityUpdates(mActivityRecognitionPendingIntent);
+                    break;
+                    *//*
+                     * An enum was added to the definition of REQUEST_TYPE,
+                     * but it doesn't match a known case. Throw an exception.
+                     *//*
+                default :
+                    try {
+                        throw new Exception("Unknown request type in onConnected().");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }*/
+
+        /*
+         * Request activity recognition updates using the preset
+         * detection interval and PendingIntent. This call is
+         * synchronous.
+         */
+        /*mActivityRecognitionClient.requestActivityUpdates(
+                DETECTION_INTERVAL_MILLISECONDS,
+                mActivityRecognitionPendingIntent);*/
+        /*
+         * Since the preceding call is synchronous, turn off the
+         * in progress flag and disconnect the client
+         */
+        /*mInProgress = false;
+        mActivityRecognitionClient.disconnect();*/
+    }
+
+    @Override
+    public void onDisconnected() {
+        // Turn off the request flag
+        mInProgress = false;
+        // Destroy the current location client
+        locationClient = null;
+        // Delete the client
+        mActivityRecognitionClient = null;
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onAddGeofencesResult(int i, String[] strings) {
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult)
+    {
+        // Turn off the request flag
+        mInProgress = false;
+
+        /*
+        * Google Play services can resolve some errors it detects.
+        * If the error has a resolution, try sending an Intent to
+        * start a Google Play services activity that can resolve
+        * error.
+        */
+        if (connectionResult.hasResolution())
+        {
+            try
+            {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                * Thrown if Google Play services canceled the original
+                * PendingIntent
+                */
+            }
+            catch (IntentSender.SendIntentException e)
+            {
+                // Log the error
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            /*
+            * If no resolution is available, display a dialog to the
+            * user with the error.
+            *//*
+            // Get the error code
+            int errorCode = connectionResult.getErrorCode();
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(errorCode, this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null)
+            {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(getSupportFragmentManager(), "Error Detection in" + getCallingActivity());*/
+            }
+        }
+
+    private class ChildAdapter extends ArrayAdapter<Child>
+    {
+        public ChildAdapter(Context context, int textViewResourceId, ArrayList<Child> items)
+        {
             super(context, textViewResourceId, items);
         }
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
             View v = convertView;
-            if(v == null){
+            if(v == null)
+            {
                 LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 v = vi.inflate(R.layout.row, null);
             }
@@ -319,6 +530,20 @@ public class LoggedInParent extends ListActivity {
     {
         Intent intent = new Intent(this, OverviewActivity.class);
         startActivity(intent);
+    }
+
+    /*
+     * Create a PendingIntent that triggers an IntentService in your
+     * app when a geofence transition occurs.
+     */
+    private PendingIntent getTransitionPendingIntent()
+    {
+        // Create an explicit Intent
+        Intent intent = new Intent(this, ReceiveTransitionsIntentService.class);
+        /*
+         * Return the PendingIntent
+         */
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     /* public void saveChildren(ArrayList<Child> myChildren)
