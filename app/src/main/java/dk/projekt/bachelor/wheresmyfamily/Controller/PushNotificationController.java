6847b478182de0e8c3456c6cb39be1dfa5d8274b
Kaare Andersen <kaareandersen07@gmail.com>
2015-01-19 16:25:55 +0100

    ui tweaks

--- app/src/main/java/dk/projekt/bachelor/wheresmyfamily/Controller/PushNotificationController.java
+++ app/src/main/java/dk/projekt/bachelor/wheresmyfamily/Controller/PushNotificationController.java
@@ -37,7 +37,7 @@ public class PushNotificationController {
         mContext = context;
         mMobileServicesClient = new MobileServicesClient(mContext);
         cEventChildController = new CEventChildController(mContext);
-        loggedInChild = new LoggedInChild();
+
     }
 
     public void getEventId(String eventID){
@@ -77,6 +77,7 @@ public class PushNotificationController {
                     wmfGeofenceController = new WmfGeofenceController();
                     wmfGeofenceController.setMyGeofences(mContext, currentGeofences);
 
+                    loggedInChild = LoggedInChild.instance;
                     loggedInChild.addGeofences();
 
                     //Convert date/month/year to int
--- app/src/main/java/dk/projekt/bachelor/wheresmyfamily/activities/LocationActivity.java
+++ app/src/main/java/dk/projekt/bachelor/wheresmyfamily/activities/LocationActivity.java
@@ -510,6 +510,7 @@ public class LocationActivity extends FragmentActivity implements
         }
 
         pushNotificationController.askForLocationFromChild(childEmail);
+        Toast.makeText(this, "Anmoder om lokation", Toast.LENGTH_LONG).show();
     }
 
     public void receiveLocation(String location){
@@ -517,6 +518,8 @@ public class LocationActivity extends FragmentActivity implements
         //String to convert
         // Bundle[{msg=ReceiveLoc ation:Loca tion[fused 56,147154,10,150219 acc=24 et=+7d15h33m53s517ms
         // alt=80.02072416373049 vel=1.6083485 bear=25.0], from=911215571794, collapse_key=do_not_collapse}]
+
+        Toast.makeText(this, "Position modtaget", Toast.LENGTH_LONG).show();
 
         StringBuilder stringBuilder = new StringBuilder(location);
         String latitudeString = stringBuilder.substring(15, 24);
